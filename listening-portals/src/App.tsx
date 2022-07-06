fdb421b9c8623178abb99d1c87946322890bf384
baitcode <batiyiv@gmail.com>
2022-07-06 21:51:10 +0100

    fixes

--- listening-portals/src/App.tsx
+++ listening-portals/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import Image from 'react-image-webp';
 import LoadingSpin from "react-loading-spin";
 import { BrowserView, MobileView } from 'react-device-detect';
+import GalleryImage from './assets/gallery.png';
 
 export interface ButtonProps {
   url: string;
@@ -104,7 +105,7 @@ function App() {
             </div>
             <div className='Buttons'>
               <div className='Stream1'>
-                <Button id="a1" url="https://listening-portals.baitcode.link/ukraine_mobile_1.mp3" />
+                <Button id="a1" url="https://listening-portals.baitcode.link/ukraine_mobile_3.mp3" />
                 <h1 className='ButtonCaption'>KYIV</h1>
               </div>
               <div className='Stream2'>
@@ -112,7 +113,7 @@ function App() {
                 <h1 className='ButtonCaption'>KHARKIV</h1>
               </div>
               <div className='Stream3'>
-                <Button id="a3" url="https://listening-portals.baitcode.link/ukraine_mobile_3.mp3" />
+                <Button id="a3" url="https://listening-portals.baitcode.link/ukraine_mobile_1.mp3" />
                 <h1 className='ButtonCaption'>ODESSA</h1>
               </div>
             </div>
@@ -120,15 +121,16 @@ function App() {
 
           <div className='English'>
             <p>
-              The work creates sound portals with the help of specially built sound boxes that were sent to three Ukrainian cities: Kyiv, Kharkiv, Odesa, Every day for 24 hours, with the help of a special mini computer and microphones located in each of the boxes, the sound of the cities will be streamed here. 
+              The work creates sound portals with the help of specially built sound boxes that were sent to three Ukrainian cities: Kyiv, Kharkiv, Odesa, Every day for 24 hours, with the help of a special mini computer and microphones located in each of the boxes, the sound of the cities will be streamed here.
             </p>
             <p>
-              “Listening portals” is an open and mobile project. It is possible for new boxes to appear in new cities and periodically to move across the territory of Ukraine.
+              “Listening portals” is an open and mobile project. It is possible for new boxes to appear in new cities and periodically to move across the territory of Ukraine. 
             </p>
             <p>
-              The work was created in collaboration with <a className="link" href="https://liamazzari.com/">Lia Mazzari</a> &#38; Ben Parry.
-              Special thanks to Grant Smith and <a className="link" href="https://soundtent.org/">Soundcamp</a> for helping build the boxes and Ilia Batii (baitcode) for technical support.
-              For help in installing boxes in Ukraine, special thanks to: Prokhorenko Rodion.
+              Project by <a className="link" href="https://www.timothymaxymenko.com/">Timothy Maxymenko</a> in collaboration with <a className="link" href="https://liamazzari.com/">Lia Mazzari</a> & Ben Parry. 
+              The project was realized with support of <a className="link" href="https://pgs.pl/">Sopot National Art Gallery</a>.
+              Special thanks to <a className="link" href="https://soundtent.org/">Grant Smith and Soundcamp</a> for helping build the boxes and Ilia Batii (baitcode) for IT and technical support. 
+              For help in installing boxes in Ukraine, special thanks to: Prokhorenko Rodion & Oleksii Chystotin.
             </p>
           </div>
         
@@ -141,23 +143,25 @@ function App() {
         <div className='Description'>
           <div className='Ukrainian'>
             <p>
-              Робота являє собою свого роду звукові портали за допомогою спеціально побудованих звукових коробок, які були відправлені до трьох українських міст: Києву, Харкова та Одеси звідки кожного дня протягом 24-х годин за допомогою спеціального міні-комп'ютера та мікрофонів розташованих у кожній із коробок йтиме звуковий лайв стрім.
+              Робота являє собою свого роду звукові портали за допомогою спеціально побудованих звукових коробок, які були відправлені до трьох українських міст: Києву, Харкова та Одеси звідки кожного дня протягом 24-х годин за допомогою спеціального міні-комп'ютера та мікрофонів розташованих у кожній із коробок йтиме звуковий лайв стрім. 
             </p>
             <p>
-              “Звукові портали” є відкритим та рухомим проектом, з часом можлива поява нових коробок у нових містах та періодичне їхнє переміщення по території України.
+              “Звукові портали” є відкритим та рухомим проектом, з часом можлива поява нових коробок у нових містах та періодичне їхнє переміщення по території України. 
             </p>
             <p>
-              Робота була створена у колаборації з <a className="link" href="https://liamazzari.com/">Лією Маццарі</a> та Беном Паррі.
-              За допомогу у встановленні боксів в Україні окрема подяка: Прохоренко Родіону.
-              Особлива подяка Grant Smith та <a className="link" href="https://soundtent.org/">Soundcamp</a> за допомогу у будівництві коробок. та Іллі
-              Батію (baitcode) за технічну підтримку.
+              Автор проєкту: <a className="link" href="https://www.timothymaxymenko.com/">Тимофій Максименко</a> у колаборації з <a className="link" href="https://liamazzari.com/">Лією Маццарі</a> та Беном Паррі.
+              Проект був реалізований за підтримки <a className="link" href="https://pgs.pl/">Національної галерея в Сопоті</a>.
+              Особлива подяка <a className="link" href="https://soundtent.org/">Grant Smith та Soundcamp</a> за допомогу у будівництві коробок. та Іллі Батію (baitcode) за IT та технічну підтримку. 
+              За допомогу у встановленні боксів в Україні окрема подяка: Прохоренко Родіону та Олексію Чистоніну. 
             </p>
           </div>
         </div>
       </div>
-
       <div className='Container'>
-        Powered by <a className="link" href="https://locusonus.org/wiki/index.php?page=Home.en">Locus Sonos</a>
+        Powered by <a className="link" href="https://locusonus.org/wiki/index.php?page=Home.en">Locus Sonus</a>
+      </div>
+      <div className='Container'>
+        <img src={GalleryImage} />
       </div>
     </div>
   );
